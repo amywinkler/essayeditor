bb0790f118810b48653fd77fb3f6b41bafcf205b
Amy Winkler <amycwinkler@gmail.com>
2015-02-14 15:15:37 -0500

    edit content.js

--- content.js
+++ content.js
@@ -1,2 +1,10 @@
+$().ready(function() {
+
 $("body").css("background-color", "red");
 console.log("hello there");
+content= $("#docs-editor-container");
+
+text= content[0].innerText;
+console.log(text);
+
+});
