f8d948d0d74a01658a1ab89e8ca170ae1f0eebeb
Amy Winkler <amycwinkler@gmail.com>
2015-02-14 21:57:09 -0500

    update content.js

--- content.js
+++ content.js
@@ -1,31 +1,48 @@
-$().ready(function() {
-
-content= $("#docs-editor-container");
-
-text= content[0].innerText;
-
-var info = new Object();
-
-var thing= ' ';
-
-var listOfStuff = text.split(" ");
-
-/*
-for (i = 0; i < text.length; i++) 
+$().ready(function() 
 {
-	ch= text.charAt(i);
- if ((ch == ' ') || (ch == '\t') || (ch == '\n'))
- {
+
+var content = $("#docs-editor-container"); //content is everything
+
+var nbsp = String.fromCharCode(160);  //chrome is dumb
+
+var text = content[0].innerText;    //text is everything but its a string now
+
+var ListOfStuff = new Array();
+
+text = text.split(nbsp); //split text into array
+
+
+   for(var i = 0; i <  text.length; i++)
+{
+var count = 0;
+
+for(var j = 0; j < text.length; j++)
+{
+
+if (text[i] ==  text[j])
+{
+count++;
+}
+console.log(text[i]);
+}
+}
+});
+
+/*for (i = 0; i < text.length; i++)        
+{
+ch = text.charAt(i)
+if ((ch == " ") || (ch == '\t') || (ch == '\n'))
+{
  listOfStuff.push(thing);
  thing= " ";
  }  
  else
  {
  thing += text.charAt(i);
- } 
+} 
 }
-*/
 
-console.log(listOfStuff.toString());
-
-});
+for (var i = 0;  i < ListOfStuff.length; i++) 
+{
+console.log(ListOfStuff[i]);
+}*/
